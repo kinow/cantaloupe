3e013b3181b03039b4135f3f0012f42351f558f0
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-07-28 23:50:42 +1200

    Issue #134 (and related to #130) re-enable test, adding a dependency to specify a different TIFFImageReader for test

--- src/test/java/edu/illinois/library/cantaloupe/processor/imageio/TIFFMetadataTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/processor/imageio/TIFFMetadataTest.java
@@ -1,35 +1,29 @@
 package edu.illinois.library.cantaloupe.processor.imageio;
 
-import edu.illinois.library.cantaloupe.operation.Orientation;
-import edu.illinois.library.cantaloupe.test.BaseTest;
-import edu.illinois.library.cantaloupe.test.TestUtil;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RIOT;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.stream.ImageInputStream;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Iterator;
 
-import static org.junit.Assert.*;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
 
-// Rationale for @Ignore:
-// See https://github.com/medusa-project/cantaloupe/issues/134
-// ImageReader has several instances. Currently, as the classloader is created in Maven,
-// the ServiceRegistry finds the implementation to be geosolutions-it/imageio-ext-tiff.
-// The implementation (TIFFImageReader) in that repository is currently using SoftReferences
-// for the image metadata. This causes this test to be quite brittle, failing intermittently.
-@Ignore
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RIOT;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.illinois.library.cantaloupe.operation.Orientation;
+import edu.illinois.library.cantaloupe.test.BaseTest;
+import edu.illinois.library.cantaloupe.test.TestUtil;
+
 public class TIFFMetadataTest extends BaseTest {
 
     private ImageReader reader;
